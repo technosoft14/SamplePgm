5b7f5797e84ceded92e8756b0764a04fb49f3f11
technosoft14 <technosoft1408@gmail.com>
2019-03-28 09:46:59 +0530

    Add My Project

---
+++ src/org/samplepgm/SampleTestedPgm.java
@@ -0,0 +1,38 @@
+package org.samplepgm;
+
+public class SampleTestedPgm {
+	public static void main(String[] args) {
+		
+	
+	//int a=10,b=20,sum,mul;
+	//sum=a+b;
+	//mul=a*b;
+	//System.out.println("sum of number" +sum);
+	//System.out.println("Mul of number"+mul);
+		//int i= 20;
+		//if(i%2==0)
+		//System.out.println("Even");
+	  //else {
+			//System.out.println("odd");
+		 //String s= "chandru";
+		 //if(s=="chandru") 
+			// System.out.println("Valid Employee");
+			 //else
+				// System.out.println("Invalid Employee");
+		//int count = 0;
+		
+		//for(int i= 1;i<=100;i++)
+		//if(i%6==0) {
+			//count++;
+			//System.out.println(count);
+		 
+		
+		 int count=0;
+		 for(int i=1;i<=10;i++)
+			 if(i%2==0)
+			 count++;
+		 System.out.println(count);
+	}}
+
+
+
