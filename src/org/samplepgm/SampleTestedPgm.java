9f02c27922129479f8c927773c97fd17ca3ee1cc
technosoft14 <technosoft1408@gmail.com>
2019-03-28 09:59:45 +0530

    modified SamplePgm

--- src/org/samplepgm/SampleTestedPgm.java
+++ src/org/samplepgm/SampleTestedPgm.java
@@ -28,8 +28,8 @@ public class SampleTestedPgm {
 		 
 		
 		 int count=0;
-		 for(int i=1;i<=10;i++)
-			 if(i%2==0)
+		 for(int i=0;i<=10;i++)
+			 if(i%2==1)
 			 count++;
 		 System.out.println(count);
 	}}
